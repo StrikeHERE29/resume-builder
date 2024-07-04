c3a40812f7d59cdd059713726aef9848950c917b
StrikeHERE29 <vasilecristian2901@yahoo.com>
2024-07-04 23:02:14 +0300

    created the function to print

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let test = document.querySelector(".test");
+    let btn = document.getElementById("printBtn");
+
+    btn.addEventListener("click", ()=>{
+        print();
+    })
+
+
+
+    
+});
